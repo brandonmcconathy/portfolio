64787090d3d97c17d7d4821c75e79ce19727bb84
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-01 11:37:00 -0700

    imported navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import Navbar from "./components/navbar"
+
 export default function Home() {
   return (
     <>
       <section>
-
+        <h1>Hero</h1>
       </section>
-      <nav>
-
-      </nav>
+      <Navbar />
       <main>
         <section>
           <h1>Projects</h1>

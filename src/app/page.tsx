2942cd47118af626be1da25f5369af0c020ee847
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-03 08:51:58 -0700

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Navbar from "./components/navbar"
 export default function Home() {
   return (
     <>
-      <section>
-        <h1>Hero</h1>
-      </section>
+      <section className="homepage-img bg-blue-400 relative select-none text-white">
+        <h1 className="text-5xl text-center absolute top-1/4 left-1/2 homepage-img-text fade-in font-semibold">Brandon McConathy</h1>
+        </section>
       <Navbar />
       <main>
         <section>

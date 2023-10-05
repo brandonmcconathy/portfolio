7f64f91dffbefd843ffc6ac2ef2202f7f751584c
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-05 15:03:43 -0700

    added more styling and content to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ export default function Home() {
   return (
     <>
       <section className="bg-blue-400 h-screen relative select-none text-white text-center">
-        <div className="absolute top-1/4 left-1/2 hero-div flex gap-10">
-          <Image src={'/brandon.JPG'} height={150} width={150} alt="Brandon McConathy profile photo" priority className="rounded-full" />
+        <div className="absolute top-1/3 left-1/2 hero-div flex items-center gap-32">
+          <Image src={'/brandon.JPG'} height={200} width={200} alt="Brandon McConathy profile photo" priority className="rounded-full box-pop" />
           <div>
             <h1 className="text-5xl font-semibold">Brandon McConathy</h1>
+            <h2 className="text-3xl my-2">Software Developer</h2>
+            <ul className="flex justify-center gap-4 text-lg">
+              <li>React</li>
+              <li>Javascript</li>
+              <li>Next</li>
+              <li>CSS</li>
+              <li>HTML</li>
+              <li>SQL</li>
+            </ul>
           </div>
         </div>
       </section>

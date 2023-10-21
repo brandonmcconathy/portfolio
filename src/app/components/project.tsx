ebb400e34033ab12c50049f1841913bf25cc095d
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-21 14:53:59 -0700

    Started styling project section

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Project(props: { title: string, imgPath: string, pageLink: string, description: string }) {
+export default function Project(props: { title: string, imgPath: string, pageLink: string, gitHubLink: string , description: string }) {
 
-  const { title, imgPath, pageLink, description } = props
+  const { title, imgPath, pageLink, gitHubLink, description } = props
 
   return(
-    <Link href={pageLink} target="_blank" className="border border-red-700 flex flex-col justify-center items-center">
-      <h1>{title}</h1>
-      <Image src={imgPath} alt="image" width={200} height={200} />
+    <div className="bg-blue-200 w-1/3 rounded-xl box-pop flex flex-col justify-center items-center">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <Image src={imgPath} alt="image" width={200} height={200} className="rounded-xl" />
       <p>{description}</p>
-    </Link>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import Navbar from "./components/navbar"
 import Project from "./components/project"
+import { projects } from '../../lib/projects'
 
 export default function Home() {
   return (
@@ -33,9 +34,9 @@ export default function Home() {
         </section>
         <div id='Projects' className="py-10"></div>
         <section className="bg-white px-10 py-4 box-pop">
-          <h1 className="text-2xl font-bold">Projects</h1>
-          <div className="flex justify-center flex-wrap">
-            <Project  title="Travis River Jones Foundation" imgPath="/projectimgs/trjfoundation-medium.png" pageLink="https://trjfoundation.vercel.app/" description="Built using React.js, Next.js, Firebase, Javascript, and Tailwind CSS."/>
+          <h1 className="text-2xl font-bold mb-5">Projects</h1>
+          <div className="flex justify-center flex-wrap gap-5">
+            {projects.map((project) => <Project title={project.title} pageLink={project.pageLink} gitHubLink={project.gitHubLink} imgPath={project.imgPath} description={project.description} key={project.id} />)}
           </div>
         </section>
         <div id='Hobbies' className="py-10"></div>
@@ -45,13 +46,13 @@ export default function Home() {
         <div id='Contact' className="py-10"></div>
         <section className="bg-white px-10 py-4 box-pop">
           <h1 className="text-2xl font-bold mb-4">Contact</h1>
-          <div className="flex flex-col items-center gap-2 text-lg mb-4 font-semibold">
+          <div className="flex flex-col items-center gap-1 text-lg mb-4 font-semibold">
             <h2>brandonmcconathy4321@gmail.com</h2>
             <h2>(909) 539-7947</h2>
           </div>
           <div className="flex justify-center gap-10 text-lg">
-            <Link href='https://github.com/brandonmcconathy' target="_blank" className="bg-black text-white rounded-lg px-3 py-1 font-semibold">Github</Link>
-            <Link href='https://www.linkedin.com/in/brandon-mcconathy-646b121a9/' target="_blank" className="linkedin-blue text-white rounded-lg px-3 py-1 font-semibold">LinkedIn</Link>
+            <Link href='https://github.com/brandonmcconathy' target="_blank" className="bg-black text-white rounded-lg px-3 py-1 font-semibold hover:bg-gray-700  transition duration-300">Github</Link>
+            <Link href='https://www.linkedin.com/in/brandon-mcconathy-646b121a9/' target="_blank" className="linkedin-blue text-white rounded-lg px-3 py-1 font-semibold hover:bg-cyan-800 transition duration-300">LinkedIn</Link>
           </div>
         </section>
       </main>

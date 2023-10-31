9e22fa52da6efe61ba4a10c2d5a641c72f1f147c
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-10-31 13:16:28 -0700

    styled hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,9 +3,9 @@ import Navbar from "./navbar"
 
 export default function Hero() {
   return(
-    <header id='top' className="bg-blue-400 h-screen select-none text-white text-center flex">
+    <header id='top' className="bg-blue-400 flex h-screen select-none text-white text-center">
         <Navbar />
-        <div className="flex items-center gap-24">
+        <div className="flex items-center gap-24 mx-auto">
           <Image src={'/brandon.JPG'} height={225} width={225} alt="Brandon McConathy profile photo" priority className="rounded-full box-pop opacity-0 fade-in" />
           <div>
             <h1 className="text-6xl font-semibold opacity-0 fade-in">Brandon McConathy</h1>

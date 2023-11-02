b99f23cee655fd06e7fc7d91194afc72ecf514f5
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-01 17:05:30 -0700

    Added nav items to navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,6 +2,11 @@ export default function Navbar() {
   return(
     <nav className="bg-blue-100 w-1/4 h-full">
       <h1>I am nav</h1>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Hobbies</li>
+      </ul>
     </nav>
   )
 }

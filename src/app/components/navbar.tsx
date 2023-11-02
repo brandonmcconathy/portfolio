c08045bdf7d0f6725a8a79211d0316aff7e300fe
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-02 14:14:47 -0700

    Started stlying navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,7 @@
 export default function Navbar() {
   return(
     <nav className="bg-blue-100 w-1/5 h-full">
-      <h1>I am nav</h1>
-      <ul>
+      <ul className="flex flex-col h-full gap-10 items-center justify-center text-black font-semibold text-xl">
         <li>About</li>
         <li>Projects</li>
         <li>Hobbies</li>

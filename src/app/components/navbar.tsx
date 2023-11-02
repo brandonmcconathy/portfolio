1f70cf04d9ded8c4531b55237b443d938975c114
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-02 14:18:41 -0700

    Styled navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return(
-    <nav className="bg-blue-100 w-1/5 h-full">
-      <ul className="flex flex-col h-full gap-10 items-center justify-center text-black font-semibold text-xl">
+    <nav className="bg-blue-100 w-1/5 h-full flex flex-col items-center justify-center">
+      <ul className="flex flex-col gap-10 text-black font-semibold text-xl">
         <li>About</li>
         <li>Projects</li>
         <li>Hobbies</li>

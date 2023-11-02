51ecdf4591db70ca39ea6c8401a69ffcb57ea92e
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-02 14:11:47 -0700

    Deleted fade in effect on hero section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,10 +6,10 @@ export default function Hero() {
     <header id='top' className="bg-blue-400 flex h-screen select-none text-white text-center">
         <Navbar />
         <div className="flex items-center gap-24 mx-auto">
-          <Image src={'/brandon.JPG'} height={225} width={225} alt="Brandon McConathy profile photo" priority className="rounded-full box-pop opacity-0 fade-in" />
+          <Image src={'/brandon.JPG'} height={225} width={225} alt="Brandon McConathy profile photo" priority className="rounded-full box-pop" />
           <div>
-            <h1 className="text-6xl font-semibold opacity-0 fade-in">Brandon McConathy</h1>
-            <h2 className="text-4xl my-2 opacity-0 fade-in delay-3000">Software Developer</h2>
+            <h1 className="text-6xl font-semibold">Brandon McConathy</h1>
+            <h2 className="text-4xl my-2">Software Developer</h2>
             <h3 className="text-lg">(909) 539-7947 | brandonmcconathy4321@gmail.com</h3>
           </div>
         </div>

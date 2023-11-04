ef41e6b065f8576309fd241356eb90567c131b39
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-04 12:25:28 -0700

    Started implementing scroll into view

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,16 @@
 export default function Navbar() {
+
+  const handleClick = (event) => {
+    console.log(event)
+    document.getElementById()?.scrollIntoView
+  }
+
   return(
     <nav className="bg-blue-100 w-1/5 h-full flex flex-col items-center justify-center">
       <ul className="flex flex-col gap-10 text-black font-semibold text-xl">
-        <li>About</li>
-        <li>Projects</li>
-        <li>Hobbies</li>
+        <li onClick={handleClick}>About</li>
+        <li onClick={handleClick}>Projects</li>
+        <li onClick={handleClick}>Hobbies</li>
       </ul>
     </nav>
   )

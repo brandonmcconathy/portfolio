4a9d8ba79eb9e1eb90a9ff2e7b4e4e08b6a8674f
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-01 16:35:18 -0700

    Moved hobbies to own file

--- src/app/components/hobbies.tsx
+++ src/app/components/hobbies.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image"
+import { hobbies } from '../../../lib/hobbies'
 
-function Hobbies(props: {title: string, imgpath: string, description: string}) {
+export default function Hobbies() {
+  return(
+    <section id="Hobbies" className="bg-white px-10 py-4 my-5 box-pop">
+      <h1 className="text-2xl font-bold mb-5">Hobbies</h1>
+      <HobbiesLeft title={hobbies[0].title} imgpath={hobbies[0].imgPath} description={hobbies[0].description} />
+      <HobbiesRight title={hobbies[1].title} imgpath={hobbies[1].imgPath} description={hobbies[1].description} />
+      <HobbiesLeft title={hobbies[2].title} imgpath={hobbies[2].imgPath} description={hobbies[2].description} />
+      <HobbiesRight title={hobbies[3].title} imgpath={hobbies[3].imgPath} description={hobbies[3].description} />
+    </section>
+  )
+}
+
+function Hobby(props: {title: string, imgpath: string, description: string}) {
 
   const { title, imgpath, description } = props
 
@@ -15,25 +28,25 @@ function Hobbies(props: {title: string, imgpath: string, description: string}) {
   )
 }
 
-export function HobbiesLeft(props: {title: string, imgpath: string, description: string}) {
+function HobbiesLeft(props: {title: string, imgpath: string, description: string}) {
 
   const { title, imgpath, description } = props
 
   return(
     <div className="bg-blue-100 flex flex-row-reverse justify-between items-center w-1/2 ml-32 my-3 p-5 box-pop rounded-xl">
-      <Hobbies title={title} imgpath={imgpath} description={description} />
+      <Hobby title={title} imgpath={imgpath} description={description} />
     </div>
   )
 }
 
-export function HobbiesRight(props: {title: string, imgpath: string, description: string}) {
+function HobbiesRight(props: {title: string, imgpath: string, description: string}) {
 
   const { title, imgpath, description } = props
   
   return(
     <div className="flex justify-end">
       <div className="bg-blue-100 flex justify-between items-center w-1/2 mr-32 my-3 p-5 box-pop rounded-xl">
-        <Hobbies title={title} imgpath={imgpath} description={description} />
+        <Hobby title={title} imgpath={imgpath} description={description} />
       </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Hero from "./components/hero"
 import About from "./components/about"
 import Projects from "./components/projects"
 import Contact from "./components/contact"
-import { HobbiesLeft, HobbiesRight } from "./components/hobbies"
-import { hobbies } from '../../lib/hobbies'
+import Hobbies from "./components/hobbies"
 
 export default function Home() {
   return (
@@ -12,13 +11,7 @@ export default function Home() {
       <main className="mb-64 text-center">
         <About />
         <Projects />
-        <section id="Hobbies" className="bg-white px-10 py-4 my-5 box-pop">
-          <h1 className="text-2xl font-bold mb-5">Hobbies</h1>
-          <HobbiesLeft title={hobbies[0].title} imgpath={hobbies[0].imgPath} description={hobbies[0].description} />
-          <HobbiesRight title={hobbies[1].title} imgpath={hobbies[1].imgPath} description={hobbies[1].description} />
-          <HobbiesLeft title={hobbies[2].title} imgpath={hobbies[2].imgPath} description={hobbies[2].description} />
-          <HobbiesRight title={hobbies[3].title} imgpath={hobbies[3].imgPath} description={hobbies[3].description} />
-        </section>
+        <Hobbies />
         <Contact />
       </main>
     </>
